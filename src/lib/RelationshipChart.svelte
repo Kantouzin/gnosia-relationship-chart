74440ee4b1cb86611bf060aba6588a7b076d9dc8
Kantouzin <kantouzin0113@gmail.com>
2024-05-02 20:02:15 +0900

    Modify chart width to be responsive

--- src/lib/RelationshipChart.svelte
+++ src/lib/RelationshipChart.svelte
@@ -2,7 +2,7 @@
   import type { Character } from '../types/character';
   import CharacterNode from './CharacterNode.svelte';
 
-  const radius: number = 300;
+  let diameter: number = 0;
   let angle: number = 0;
 
   const fetchCharacters = async (): Promise<Character[]> => {
@@ -15,14 +15,31 @@
 </script>
 
 <div
-  class="border border-5 rounded-circle d-flex justify-content-center align-items-center my-5"
-  style="width: {radius * 2}px; aspect-ratio: 1 / 1;"
+  bind:offsetWidth={diameter}
+  class="chart border border-5 rounded-circle d-flex justify-content-center align-items-center my-5"
 >
   {#await fetchCharacters()}
     <p>Now Loading ...</p>
   {:then characters}
     {#each characters as character, index}
-      <CharacterNode {character} angle={angle * index} {radius} />
+      <CharacterNode
+        {character}
+        angle={angle * index}
+        radius={diameter * 0.5}
+      />
     {/each}
   {/await}
 </div>
+
+<style>
+  .chart {
+    aspect-ratio: 1 / 1;
+    width: 85%;
+  }
+
+  @media (min-width: 768px) {
+    .chart {
+      width: 50%;
+    }
+  }
+</style>
